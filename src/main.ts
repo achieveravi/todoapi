9fdbe0f9316547a54e0d6e96517c8399b3e757a3
Ravi Mishra <ravi.achieve@gmail.com>
2022-11-04 18:13:13 +0530

    #1 Integrating swagger with the api

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ToDo Api')
+    .setDescription('A rest api to manage ToDos')
+    .setVersion('1.0')
+    .addTag('ToDo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
